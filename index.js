43534780649910cf4271bbb7a3c2a8e56dd10563
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-16 14:50:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ const client = new Client({
 client.once("ready", async () => {
   console.log("Bot is ready");
 
-  // ضع هنا ID القناة
-  const channel = await client.channels.fetch("1516072447219335415");
+  try {
+    const channel = await client.channels.fetch("1516072447219335415");
 
-  const embed = new EmbedBuilder()
-    .setTitle("📖 رسالة اختبار")
-    .setDescription("إذا رأيت هذه الرسالة فهذا يعني أن البوت يعمل بشكل صحيح ✔")
-    .setColor(0xFFD700)
-    .setFooter({ text: "بوت الأحاديث" });
+    const embed = new EmbedBuilder()
+      .setTitle("📖 اختبار البوت")
+      .setDescription("البوت يعمل بشكل صحيح ✔");
 
-  await channel.send({ embeds: [embed] });
+    await channel.send({ embeds: [embed] });
+
+  } catch (err) {
+    console.error("Error:", err);
+  }
 });
 
 client.login(process.env.TOKEN);
