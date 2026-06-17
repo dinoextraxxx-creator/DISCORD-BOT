317446d7c03acce99ec7f8a6671500e829e8d076
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-17 10:51:07 +0100

    Update hadithSystem.js

--- hadithSystem.js
+++ hadithSystem.js
@@ -4,10 +4,9 @@ const { EmbedBuilder } = require("discord.js");
 const CHANNEL = "1516016586643734639";
 
 const ICON =
-"https://cdn.discordapp.com/attachments/1515161056975126705/1515903883430465647/-_1.jpg?ex=6a30b301&is=6a2f6181&hm=99212fa7d1a01c5bd6253cacfb49d1b849226abffe617b60c1c53121e1805f0f&";
+"https://cdn.discordapp.com/attachments/1515161056975126705/1515903883430465647/-_1.jpg";
 
 const AUTHOR = "مُـــذَكّــــــر | مواعـــيد الصــــلاة";
-
 const FOOTER = "4KO • YONKO.مُـــذَكّــــــر";
 
 const TITLE =
@@ -17,7 +16,6 @@ const data = JSON.parse(fs.readFileSync("./hadiths.json","utf8"));
 const hadiths = data.hadiths;
 
 let used = new Set();
-let started = false;
 
 function pick(){
 
@@ -37,31 +35,22 @@ return hadiths[i];
 function embed(h){
 
 return new EmbedBuilder()
-
 .setColor("#FFD700")
-
 .setAuthor({
 name: AUTHOR,
 iconURL: ICON
 })
-
 .setTitle(TITLE)
-
-.setDescription(`«${h.text}»`)
-
+.setDescription(`«${h.text}»\n\n${h.narrator}\n\n${h.source}`)
 .setFooter({
 text: FOOTER,
 iconURL: ICON
 })
-
 .setTimestamp();
 
 }
 
-async function loop(client){
-
-if(started) return;
-started = true;
+async function startHadithSystem(client){
 
 const ch = await client.channels.fetch(CHANNEL);
 
@@ -71,14 +60,11 @@ await ch.send({
 embeds:[embed(pick())]
 });
 
-setTimeout(send, 120000);
+setTimeout(send, 120000); // كل دقيقتين
+
 }
 
 send();
-}
-
-async function startHadithSystem(client){
-await loop(client);
 }
 
 module.exports = { startHadithSystem };
