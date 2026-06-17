595dcd98634c809c21afed6c6eed7520afe7f5e6
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-17 15:16:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const PRAYER_CHANNEL = "1516405973365952633";
 client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
 
-  // 📖 نظام الأحاديث
+  // 📖 الأحاديث (كل دقيقتين)
   const hadithBot = new HadithSystem(client, HADITH_CHANNEL, hadiths);
   hadithBot.start();
 
-  // 🕌 نظام الصلاة
+  // 🕌 الصلاة (فوري + دقيقة بين كل صلاة + بدون تكرار)
   const prayerBot = new PrayerSystem(client, PRAYER_CHANNEL);
-  await prayerBot.start();
+  prayerBot.start();
 });
 
 client.login("PUT_YOUR_BOT_TOKEN_HERE");
