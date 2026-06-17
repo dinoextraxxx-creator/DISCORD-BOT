b898ec4d56499b1fef5eaf7e6649b0d337e494fe
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-17 14:49:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { startHadithSystem } = require("./hadithSystem");
+const { startPrayerSystem } = require("./prayerSystem");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  startHadithSystem(client);
+  startPrayerSystem(client);
+});
+
+client.login(process.env.TOKEN);
