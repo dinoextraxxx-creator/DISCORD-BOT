5f60626df0d40084110c16f8f70d87e886a3b296
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-17 15:00:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const HadithSystem = require("./hadithSystem");
+const PrayerSystem = require("./prayerSystem");
+const hadiths = require("./hadiths.json");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+// IDs القنوات
+const HADITH_CHANNEL = "1516016586643734639";
+const PRAYER_CHANNEL = "1516405973365952633";
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  // تشغيل نظام الأحاديث
+  const hadithBot = new HadithSystem(client, HADITH_CHANNEL, hadiths);
+  hadithBot.start();
+
+  // تشغيل نظام الصلاة
+  const prayerBot = new PrayerSystem(client, PRAYER_CHANNEL);
+  prayerBot.start();
+});
+
+client.login("PUT_YOUR_BOT_TOKEN_HERE");
