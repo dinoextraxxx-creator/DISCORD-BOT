b0bb7593e19a8597535e148b482738f26c60b61b
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-17 15:25:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,11 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-// تشغيل عند جاهزية البوت
 client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
 
-  // تشغيل نظام الحديث
   hadithSystem.start(client);
-
-  // تشغيل نظام الصلاة
   prayerSystem.start(client);
 });
 
-// توكن البوت
 client.login(process.env.TOKEN);
