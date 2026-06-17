56ad4254b7c3664be59970fc1ce85ac3266f22d1
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-17 17:39:33 +0100

    Update prayers.js

--- prayers.js
+++ prayers.js
@@ -1,1 +1,22 @@
-
+module.exports = [
+  {
+    name: "الفجر",
+    timeKey: "fajr"
+  },
+  {
+    name: "الظهر",
+    timeKey: "dhuhr"
+  },
+  {
+    name: "العصر",
+    timeKey: "asr"
+  },
+  {
+    name: "المغرب",
+    timeKey: "maghrib"
+  },
+  {
+    name: "العشاء",
+    timeKey: "isha"
+  }
+];
