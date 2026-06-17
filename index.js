4792cbbdeb4daf9436c003d394408c1fb53adda9
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-17 05:23:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-client.once("ready", () => {
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const { startPrayerSystem } = require("./prayerSystem");
+const { startHadithSystem } = require("./hadithSystem");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+process.on("unhandledRejection", console.log);
+process.on("uncaughtException", console.log);
+
+client.once("ready", async () => {
   console.log("BOT READY");
 
   startPrayerSystem(client);
   startHadithSystem(client);
 });
+
+client.login(process.env.TOKEN);
