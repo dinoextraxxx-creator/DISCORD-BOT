6049c4f95b0f2d461a2ec88699b254b8e2970268
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-18 08:17:59 +0100

    Update hadithSystem.js

--- hadithSystem.js
+++ hadithSystem.js
@@ -1,27 +1,76 @@
-const { EmbedBuilder } = require("discord.js");
+const format=require("./hadithFormatter");
 
-module.exports = function(h,color,icon){
+const p1=require("./hadiths_part1");
+const p2=require("./hadiths_part2");
+const p3=require("./hadiths_part3");
+const p4=require("./hadiths_part4");
+const p5=require("./hadiths_part5");
+const p6=require("./hadiths_part6");
+const p7=require("./hadiths_part7");
+const p8=require("./hadiths_part8");
+const p9=require("./hadiths_part9");
+const p10=require("./hadiths_part10");
 
-let text=`🔸 قال رسول الله ﷺ: «${h.text}»`;
+const ALL=[
+...p1,...p2,...p3,...p4,...p5,
+...p6,...p7,...p8,...p9,...p10
+];
 
-text+=`\n\n👤 الراوي : ${h.rawi}`;
-text+=`\n📚 المصدر : ${h.source}`;
+let last=null;
 
-if(h.bayan){
-text+=`\n📖 بيان : ${h.bayan}`;
+function pick(){
+
+let h;
+
+do{
+h=ALL[Math.floor(Math.random()*ALL.length)];
+}
+while(last && h.text===last.text);
+
+last=h;
+
+return h;
+
 }
 
-return new EmbedBuilder()
-.setColor(color||"#FF0000")
-.setAuthor({
-name:"مُـــذَكّــــــر",
-iconURL:icon
-})
-.setDescription(text)
-.setFooter({
-text:"4KO • YONKO.مُـــذَكّــــــر",
-iconURL:icon
-})
-.setTimestamp();
+async function startHadithSystem(client,opt){
 
-};
+const channel=
+await client.channels.fetch(
+opt.channelId
+);
+
+if(!channel)return;
+
+async function send(){
+
+const h=pick();
+
+const embed=
+format(
+h,
+opt.color,
+opt.icon
+);
+
+await channel.send({
+embeds:[embed]
+});
+
+}
+
+setTimeout(()=>{
+
+send();
+
+setInterval(
+send,
+150*60*1000
+);
+
+},150*60*1000);
+
+}
+
+module.exports=
+startHadithSystem;
