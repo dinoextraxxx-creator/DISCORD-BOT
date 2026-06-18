7fc0bb8bc2f83e5ab0b6f727dadd650fdd969c25
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-18 01:45:00 +0100

    Update hadithFormatter.js

--- hadithFormatter.js
+++ hadithFormatter.js
@@ -1,1 +1,23 @@
+/**
+ * 📖 Hadith Formatter System
+ * مسؤول عن تنسيق الحديث داخل Embed بشكل موحد
+ */
 
+function formatHadith(hadith) {
+  const base = `
+🔸 ➤ **قال رسول الله ﷺ:** «${hadith.text}»
+
+👤 ➤ **الراوي:** ${hadith.rawi}
+
+📚 ➤ **المصدر:** ${hadith.source}
+`;
+
+  // إضافة "بيان" فقط إذا كان موجود
+  const bayanPart = hadith.bayan
+    ? `\n📖 ➤ **بيان:** ${hadith.bayan}\n`
+    : "";
+
+  return base + bayanPart;
+}
+
+module.exports = { formatHadith };
