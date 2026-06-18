e5ee761d7663584d708204517e10bad26fbc146f
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-18 08:29:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,39 +23,241 @@ GatewayIntentBits.Guilds
 
 });
 
+// 🟡 أيقونة الصلاة
 const ICON_PRAYER=
 "https://cdn.discordapp.com/attachments/1515161056975126705/1516909922040811610/-_4.jpg";
 
+// 🔴 أيقونة الأحاديث
 const ICON_HADITH=
 "https://cdn.discordapp.com/attachments/1515161056975126705/1515903883430465647/-_1.jpg";
 
+const AZKAR=`
+1- يقول مثل ما يقول المؤذن إلا في "حي على الصلاة و حي على الفلاح" فيقول "لا حول ولا قوة إلا بالله"
+
+2- يقول "وأنا أشهد أن لا إله إلا الله، وحده لا شريك له، وأن محمد عبده ورسوله، رضيت بالله ربًا، وبمحمدٍ رسولًا وبالإسلام دينًا"
+
+3- يصلي على النبي -صلى الله عليه وسلم- بعد فراغه من إجابة المؤذن
+
+4- اللهم رب هذه الدعوة التامة، والصلاة القائمة، آت محمدًا الوسيلة والفضيلة، وابعثه مقامًا محمودًا الذي وعدته، [ إنك لا تخلف الميعاد ]
+
+5- يدعو لنفسه بين الأذان والإقامة فإن الدعاء حينئذٍ لا يرد
+`;
+
 client.once(
 Events.ClientReady,
+
 async()=>{
 
 console.log(
 "BOT READY"
 );
 
+// 🟡 تشغيل الصلاة
+try{
+
 await startPrayerSystem(
 client
 );
 
+}catch(e){
+
+console.log(
+"Prayer:",
+e
+);
+
+}
+
+// 🔴 تشغيل الأحاديث
+try{
+
 await startHadithSystem(
+
 client,
+
 {
+
+// ✅ قناة الأحاديث الصحيحة
 channelId:
-"1516405973365952633",
+"1516016586643734639",
 
 icon:
 ICON_HADITH,
 
 color:
 "#FF0000"
+
 }
+
 );
 
+}catch(e){
+
+console.log(
+"Hadith:",
+e
+);
+
+}
+
+}
+
+);
+
+client.on(
+Events.InteractionCreate,
+
+async(i)=>{
+
+if(
+!i.isButton()
+)
+return;
+
+try{
+
+if(
+i.customId.startsWith(
+"pray_"
+)
+){
+
+const prayer=
+i.customId.replace(
+"pray_",
+""
+);
+
+return i.reply({
+
+ephemeral:true,
+
+embeds:[
+
+new EmbedBuilder()
+
+.setColor(
+"#00FF66"
+)
+
+.setAuthor({
+
+name:
+"مُـــذَكّــــــر",
+
+iconURL:
+ICON_PRAYER
+
+})
+
+.setDescription(
+
+PRAYER_DETAILS?.[
+prayer
+]
+
+||
+
+"لا توجد بيانات"
+
+)
+
+.setFooter({
+
+text:
+"4KO • YONKO.مُـــذَكّــــــر",
+
+iconURL:
+ICON_PRAYER
+
+})
+
+.setTimestamp()
+
+]
+
 });
+
+}
+
+if(
+i.customId==="azkar"
+){
+
+return i.reply({
+
+ephemeral:true,
+
+embeds:[
+
+new EmbedBuilder()
+
+.setColor(
+"#00FF66"
+)
+
+.setAuthor({
+
+name:
+"مُـــذَكّــــــر",
+
+iconURL:
+ICON_PRAYER
+
+})
+
+.setTitle(
+"اذكـــــــار الصــــلاة"
+)
+
+.setDescription(
+AZKAR
+)
+
+.setFooter({
+
+text:
+"4KO • YONKO.مُـــذَكّــــــر",
+
+iconURL:
+ICON_PRAYER
+
+})
+
+.setTimestamp()
+
+]
+
+});
+
+}
+
+}catch(err){
+
+console.log(
+err
+);
+
+if(
+!i.replied
+){
+
+i.reply({
+
+ephemeral:true,
+
+content:
+"خطأ"
+
+});
+
+}
+
+}
+
+}
+
+);
 
 client.login(
 process.env.TOKEN
