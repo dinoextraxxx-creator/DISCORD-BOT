9778cd834b65dd1fa98c402a193a6e8e81029fe8
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-18 03:08:42 +0100

    Update hadithSystem.js

--- hadithSystem.js
+++ hadithSystem.js
@@ -1,91 +1,62 @@
-const formatter = require("./hadithFormatter");
+const formatter=require("./hadithFormatter");
 
-const CHANNEL_ID =
-"1516016586643734639";
+const CHANNEL_ID="1516016586643734639";
 
-const CHANGE_INTERVAL =
-120000;
+const INTERVAL=120000;
 
-const parts = [
+// تحميل كل الأجزاء بشكل صحيح (بدون .partX)
+const part1=require("./hadiths_part1");
+const part2=require("./hadiths_part2");
+const part3=require("./hadiths_part3");
+const part4=require("./hadiths_part4");
+const part5=require("./hadiths_part5");
+const part6=require("./hadiths_part6");
+const part7=require("./hadiths_part7");
+const part8=require("./hadiths_part8");
+const part9=require("./hadiths_part9");
+const part10=require("./hadiths_part10");
 
-...require("./hadiths_part1").part1,
-
-...require("./hadiths_part2").part2,
-
-...require("./hadiths_part3").part3,
-
-...require("./hadiths_part4").part4,
-
-...require("./hadiths_part5").part5,
-
-...require("./hadiths_part6").part6,
-
-...require("./hadiths_part7").part7,
-
-...require("./hadiths_part8").part8,
-
-...require("./hadiths_part9").part9,
-
-...require("./hadiths_part10").part10
-
+// دمج كل الأحاديث
+const hadiths=[
+...part1,
+...part2,
+...part3,
+...part4,
+...part5,
+...part6,
+...part7,
+...part8,
+...part9,
+...part10
 ];
 
-module.exports = async (client)=>{
+module.exports=async(client)=>{
 
-let lastIndex = -1;
+let last=-1;
 
-async function sendRandom(){
+const send=async()=>{
 
-const channel =
-await client.channels.fetch(
-CHANNEL_ID
-);
-
-if(!channel)
-return;
+const channel=await client.channels.fetch(CHANNEL_ID);
+if(!channel)return;
 
 let index;
 
 do{
+index=Math.floor(Math.random()*hadiths.length);
+}while(index===last && hadiths.length>1);
 
-index =
-Math.floor(
-Math.random()
-*
-parts.length
-);
-
-}
-
-while(
-index === lastIndex
-&&
-parts.length > 1
-);
-
-lastIndex =
-index;
-
-const hadith =
-parts[index];
+last=index;
 
 await channel.send({
-
-embeds:[
-formatter(
-hadith
-)
-]
-
+embeds:[formatter(hadiths[index])]
 });
 
-}
+};
 
-await sendRandom();
+// أول إرسال مباشر
+await send();
 
-setInterval(
-sendRandom,
-CHANGE_INTERVAL
-);
+// كل دقيقتين
+setInterval(send,INTERVAL);
 
 };
