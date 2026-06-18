acfc9737950ed3deb8613cbf83afcd12a9392063
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-18 03:29:00 +0100

    Update hadithSystem.js

--- hadithSystem.js
+++ hadithSystem.js
@@ -2,9 +2,13 @@ const formatter=require("./hadithFormatter");
 
 const CHANNEL_ID="1516016586643734639";
 
-const INTERVAL=120000;
+const SCHEDULE=[
+{h:7,m:15},
+{h:12,m:0},
+{h:18,m:15},
+{h:21,m:30}
+];
 
-// تحميل كل الأجزاء بشكل صحيح (بدون .partX)
 const part1=require("./hadiths_part1");
 const part2=require("./hadiths_part2");
 const part3=require("./hadiths_part3");
@@ -16,47 +20,60 @@ const part8=require("./hadiths_part8");
 const part9=require("./hadiths_part9");
 const part10=require("./hadiths_part10");
 
-// دمج كل الأحاديث
 const hadiths=[
-...part1,
-...part2,
-...part3,
-...part4,
-...part5,
-...part6,
-...part7,
-...part8,
-...part9,
-...part10
+...part1,...part2,...part3,...part4,...part5,
+...part6,...part7,...part8,...part9,...part10
 ];
 
-module.exports=async(client)=>{
+module.exports=(client,options={})=>{
+
+const icon=options.icon;
+const color=options.color||"#FFA500";
 
 let last=-1;
+let sent=new Set();
 
-const send=async()=>{
-
-const channel=await client.channels.fetch(CHANNEL_ID);
-if(!channel)return;
-
-let index;
-
+function randomHadith(){
+let i;
 do{
-index=Math.floor(Math.random()*hadiths.length);
-}while(index===last && hadiths.length>1);
+i=Math.floor(Math.random()*hadiths.length);
+}while(i===last && hadiths.length>1);
+last=i;
+return hadiths[i];
+}
 
-last=index;
+async function send(){
+const ch=await client.channels.fetch(CHANNEL_ID);
+if(!ch)return;
+await ch.send({embeds:[formatter(randomHadith(),color,icon)]});
+}
 
-await channel.send({
-embeds:[formatter(hadiths[index])]
-});
+function tick(){
 
-};
+const now=new Date();
+const time=new Date(now.toLocaleString("en-US",{timeZone:"Africa/Casablanca"}));
 
-// أول إرسال مباشر
-await send();
+const h=time.getHours();
+const m=time.getMinutes();
 
-// كل دقيقتين
-setInterval(send,INTERVAL);
+const key=`${h}:${m}`;
 
+if(sent.has(key))return;
+
+for(const s of SCHEDULE){
+if(s.h===h && s.m===m){
+send();
+sent.add(key);
+}
+}
+}
+
+function reset(){
+sent.clear();
+}
+
+setInterval(tick,60000);
+setInterval(reset,86400000);
+
+console.log("Hadith system running...");
 };
