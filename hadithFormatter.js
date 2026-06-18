f827425cb4e9a56306de9e806a284ac768e6f07f
dinoextraxxx-creator <dinoextraxxx@gmail.com>
2026-06-18 08:22:40 +0100

    Update hadithFormatter.js

--- hadithFormatter.js
+++ hadithFormatter.js
@@ -12,16 +12,26 @@ text+=`\n📖 بيان : ${h.bayan}`;
 }
 
 return new EmbedBuilder()
-.setColor(color||"#FF0000")
+
+.setColor(color || "#FF0000")
+
 .setAuthor({
 name:"مُـــذَكّــــــر",
 iconURL:icon
 })
+
+// ✅ العنوان المضاف
+.setTitle(
+"﴿ وَذَكِّرْ فَإِنَّ الذِّكْرَىٰ تَنفَعُ الْمُؤْمِنِينَ﴾"
+)
+
 .setDescription(text)
+
 .setFooter({
 text:"4KO • YONKO.مُـــذَكّــــــر",
 iconURL:icon
 })
+
 .setTimestamp();
 
 };
